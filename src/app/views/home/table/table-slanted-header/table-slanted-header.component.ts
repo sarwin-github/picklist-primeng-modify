fb2255d72548e637f1f6adbdba67aafb6b809b7b
Sherwin Dapito <dapito.sherwin@gmail.com>
2020-01-12 10:35:06 +0800

    added custom resize

--- src/app/views/home/table/table-slanted-header/table-slanted-header.component.ts
+++ src/app/views/home/table/table-slanted-header/table-slanted-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer } from '@angular/core';
 import { Car } from '../../../../shared/domain/car';
 import { CarService } from '../../../../shared/services/car/car.service';
 import { mainAnimations } from '../../../../shared/animations/main-animations';
@@ -16,7 +16,8 @@ export class TableSlantedHeaderComponent implements OnInit {
 
     public slanted: boolean;
 
-  	constructor(private carService: CarService) { }
+  	constructor(private carService: CarService,
+        private renderer: Renderer) { }
 
     ngOnInit() {
         this.carService.getCarsSmall().then(cars => {
@@ -28,10 +29,10 @@ export class TableSlantedHeaderComponent implements OnInit {
         });
 
         this.cols = [
-            { field: 'vin', header: 'Vin' },
-            { field: 'year', header: 'Year' },
-            { field: 'brand', header: 'Brand' },
-            { field: 'color', header: 'Color' }
+            { field: 'vin', header: 'Vin', width: '40px' },
+            { field: 'year', header: 'Year', width: '40px' },
+            { field: 'brand', header: 'Brand', width: '40px' },
+            { field: 'color', header: 'Color', width: '40px' }
         ];
 
         this.slanted = true;
@@ -50,5 +51,40 @@ export class TableSlantedHeaderComponent implements OnInit {
             elements[0].classList.remove('ui-state-highlight')
         */
     }
+
+    start: any = undefined;
+    pressed: boolean = false;
+    startX: any;
+    startWidth: any;
+    resizableFnMousemove:any;
+    resizableFnMouseup: any;
+
+    resizeTable(event: any, column: any) {
+      this.start = event.target;
+      this.pressed = true;
+      this.startX = event.pageX;
+      this.startWidth = this.start.clientWidth;
+      this.mouseMove(column);
+      console.log(column)
+    }
+
+    mouseMove(column: any) {
+      this.resizableFnMousemove = this.renderer.listen('document', 'mousemove', (event) => {
+        if (this.pressed) {
+          let width = this.startWidth + (event.pageX - this.startX);
+          let index = this.start.cellIndex;
+          column.width = width;
+        }
+      });
+
+
+      this.resizableFnMouseup = this.renderer.listen('document', 'mouseup', (event) => {
+        if (this.pressed) {
+          this.pressed = false;
+          this.resizableFnMousemove();
+          this.resizableFnMouseup();
+        }
+      });
+    };
 
 }
